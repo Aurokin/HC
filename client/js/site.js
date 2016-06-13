0112284c5ad4612e1d5dd6910446e3fde8d176c9
Hunter Sadler <hsadlersemail@gmail.com>
2016-06-13 01:36:50 -0500

    Home Page: Made Columns Larger, Background Image Applied To Div Not Body Admin Panel: Created, Create / Edit / Delete Posts Package: Add LESS, Moment, Markdown Posts: Parse Text As Markdown

--- client/js/site.js
+++ client/js/site.js
@@ -1,3 +1,9 @@
+Date.prototype.toDateInputValue = (function() {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0,10);
+});
+
 recruitmentHoverOn = function() {
   var x = $(this).offset().left;
   var y = $(this).offset().top;
@@ -12,12 +18,67 @@ recruitmentHoverOff = function() {
   $("#" + spec).addClass("hidden");
 }
 
+Template.registerHelper('formatDate', function(date) {
+  return moment(date).format('MM/DD/YYYY');
+});
+
 Template.blogHome.helpers({
   posts: function() {
-    return Posts.find();
+    return Posts.find({}, {sort: {date: -1} });
   }
 });
 
 Template.header.rendered = function() {
   $('.classIcon').hover(recruitmentHoverOn, recruitmentHoverOff);
 };
+
+Template.adminContent.helpers({
+  posts: function() {
+    return Posts.find({}, {sort: {date: -1} });
+  }
+});
+
+Template.adminContent.rendered = function() {
+  $('#newPostDate').val(new Date().toDateInputValue());
+};
+
+Template.adminContent.events({
+  'submit #newPost'(event) {
+    event.preventDefault();
+    const target = event.target;
+    const header = target.newPostHeader.value;
+    const image = target.newPostImage.value;
+    const text = target.newPostText.value;
+    const date = target.newPostDate.value;
+    Posts.insert({
+      header,
+      image,
+      text,
+      date,
+      createdAt: new Date(),
+    });
+    target.newPostHeader.value = '';
+    target.newPostImage.value = '';
+    target.newPostText.value = '';
+    target.newPostDate.value = new Date().toDateInputValue();
+  },
+
+  'submit .currentPost'(event) {
+    event.preventDefault();
+    const target = event.target;
+    const header = $("#" + this._id + "-PostHeader").val();
+    const image = $("#" + this._id + "-PostImage").val();
+    const text = $("#" + this._id + "-PostText").val();
+    const date = $("#" + this._id + "-PostDate").val();
+    Posts.update(this._id, {
+      header,
+      image,
+      text,
+      date,
+    });
+  },
+
+  'click .deletePost'() {
+    Posts.remove(this._id);
+  },
+});
